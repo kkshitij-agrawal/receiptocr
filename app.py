76e371639ab0e287589c9d916e26e01d825174f0
Kshitij Agrawal <43761593+kkshitij-agrawal@users.noreply.github.com>
2023-07-30 14:17:25 +0100

    using ocr.space instead

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 
 from flask import Flask, render_template, request, jsonify, send_from_directory
-from PIL import Image, ImageEnhance, ImageFilter, ImageOps
-import pytesseract
-import cv2
-import numpy as np
-import os
+import requests
 
 app = Flask(__name__)
 
-# Directory to save pre-processed images
-PROCESSED_IMAGE_DIR = 'processed_images'
-if not os.path.exists(PROCESSED_IMAGE_DIR):
-    os.makedirs(PROCESSED_IMAGE_DIR)
+# Directory to save uploaded images for OCR processing
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 @app.route('/')
 def index():
@@ -25,30 +21,24 @@ def upload_file():
     if file.filename == '':
         return jsonify({"error": "No selected file"})
     if file:
-        image = Image.open(file.stream)
-        
-        # Image pre-processing using OpenCV
-        image_np = np.array(image)
-        gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
-        denoised = cv2.fastNlMeansDenoising(gray, None, 30, 7, 21)
-        adaptive_thresh = cv2.adaptiveThreshold(denoised, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-        kernel = np.ones((2,2), np.uint8)
-        morphed = cv2.morphologyEx(adaptive_thresh, cv2.MORPH_CLOSE, kernel)
-        scaled = cv2.resize(morphed, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
-        
-        # Convert back to PIL Image and save for display
-        processed_image = Image.fromarray(scaled)
-        processed_image_path = os.path.join(PROCESSED_IMAGE_DIR, 'processed.png')
-        processed_image.save(processed_image_path)
-        
-        # Extract text using Tesseract with specific configuration
-        custom_config = r'--oem 3 --psm 6'  # Use LSTM OCR Engine and assume a uniform block of text
-        text = pytesseract.image_to_string(processed_image, lang='eng', config=custom_config)
-        return jsonify({"extracted_text": text, "processed_image_path": processed_image_path})
+        # Save the uploaded file
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(file_path)
 
-@app.route('/processed_image/<filename>')
-def processed_image(filename):
-    return send_from_directory(PROCESSED_IMAGE_DIR, filename)
+        # Use OCR.space API to process the image
+        url = "https://api.ocr.space/parse/image"
+        api_key = "K86447298988957"  # Replace with your API key
+        with open(file_path, 'rb') as image_file:
+            response = requests.post(url,
+                                     files={"file": image_file},
+                                     data={"apikey": api_key, "language": "eng"})
+        
+        # Extract text from the OCR.space API response
+        result = response.json()
+        if result['IsErroredOnProcessing']:
+            return jsonify({"error": "Error processing the image with OCR.space API"})
+        extracted_text = result['ParsedResults'][0]['ParsedText']
+        return jsonify({"extracted_text": extracted_text})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
