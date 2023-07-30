0b315f1f5a3f553e5bd4d7ef74150a0c04421264
Kshitij Agrawal <43761593+kkshitij-agrawal@users.noreply.github.com>
2023-07-30 13:11:57 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template, request, jsonify
-from PIL import Image, ImageEnhance, ImageFilter
+
+from flask import Flask, render_template, request, jsonify, send_from_directory
+from PIL import Image, ImageEnhance, ImageFilter, ImageOps
 import pytesseract
+import cv2
+import numpy as np
+import os
 
 app = Flask(__name__)
+
+# Directory to save pre-processed images
+PROCESSED_IMAGE_DIR = 'processed_images'
+if not os.path.exists(PROCESSED_IMAGE_DIR):
+    os.makedirs(PROCESSED_IMAGE_DIR)
 
 @app.route('/')
 def index():
@@ -18,13 +27,24 @@ def upload_file():
     if file:
         image = Image.open(file.stream)
         
-        # Image pre-processing
-        image = image.convert('L')  # Convert to grayscale
-        image = image.point(lambda x: 0 if x < 128 else 255, '1')  # Binarization
-        image = image.resize((int(image.width * 2), int(image.height * 2)))  # Resize for better accuracy
+        # Image pre-processing using OpenCV
+        image_np = np.array(image)
+        gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
+        adaptive_thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        kernel = np.ones((2,2), np.uint8)
+        morphed = cv2.morphologyEx(adaptive_thresh, cv2.MORPH_CLOSE, kernel)
         
-        text = pytesseract.image_to_string(image, lang='eng')
-        return jsonify({"extracted_text": text})
+        # Convert back to PIL Image and save for display
+        processed_image = Image.fromarray(morphed)
+        processed_image_path = os.path.join(PROCESSED_IMAGE_DIR, 'processed.png')
+        processed_image.save(processed_image_path)
+        
+        text = pytesseract.image_to_string(processed_image, lang='eng')
+        return jsonify({"extracted_text": text, "processed_image_path": processed_image_path})
+
+@app.route('/processed_image/<filename>')
+def processed_image(filename):
+    return send_from_directory(PROCESSED_IMAGE_DIR, filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
